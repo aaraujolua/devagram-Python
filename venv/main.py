8ef1b5859d3b225da5c61186c35cd5b129b4552b
Luana Araújo <f.araujoluana@gmail.com>
2024-01-03 19:44:17 -0300

    UPDATE main.py

--- venv/main.py
+++ venv/main.py
@@ -1,1 +1,16 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
+from typing import Optional
+
+class User(BaseModel):
+    name: str
+    user: Optional[str] = None
+    password: str
+
+app = FastAPI()
+
+@app.get("/api/health", tags=["Health"])
+async def health():
+    return {
+        "status": "OK!"
+    }
