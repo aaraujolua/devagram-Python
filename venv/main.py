86a3406e4e964fc62e2f1a36bc972a1bc32d171d
Luana Araújo <f.araujoluana@gmail.com>
2024-01-04 17:01:37 -0300

    UPDATE main.py

--- venv/main.py
+++ venv/main.py
@@ -4,12 +4,6 @@ from routes.UserRoute import router as UserRoute #"as" apenas altera o nome do q
 from pydantic import BaseModel
 from typing import Optional
 
-
-#class User(BaseModel):
-    #name: str
-    #user: Optional[str] = None
-    #password: str
-
 app = FastAPI()
 
 app.include_router(UserRoute, tags=["User"], prefix="/api/user")
