21cf47564b707e720e2c7b1f7fe2c1613da53ef6
Luana Araújo <f.araujoluana@gmail.com>
2024-01-08 21:30:16 -0300

    UPDATE UserModel.py

--- models/UserModel.py
+++ models/UserModel.py
@@ -1,19 +1,36 @@
 from pydantic import BaseModel, Field, EmailStr
 
 class UserModel(BaseModel):
+    id: str = Field(...)
     name: str = Field(...)
     email: EmailStr = Field(...)  
     password: str = Field (...)
     icon: str = Field(...)
     
     
-class Config:
-    extra_schema = {
-        "user": {
-                "name": "Luana",
-                "email": "f.araujoluana@gmail.com",
-                "password": "123lua",
-                "icon": "lua.jpg"
+    class Config:
+        extra_schema = {
+            "user": {
+                    "name": "Luana",
+                    "email": "f.araujoluana@gmail.com",
+                    "password": "123lua",
+                    "icon": "lua.jpg"
+            }
         }
-    }
+
+
+class UserCreateModel(BaseModel):
+    name: str = Field(...)
+    email: EmailStr = Field(...)  
+    password: str = Field (...)
+    icon: str = Field(...)
     
+    class Config:
+        extra_schema = {
+            "user": {
+                    "name": "Luana",
+                    "email": "f.araujoluana@gmail.com",
+                    "password": "123lua",
+                    "icon": "lua.jpg"
+            }
+        }
