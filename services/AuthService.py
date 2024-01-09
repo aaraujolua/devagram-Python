25c766e1ddce56fad717b7a325457371f141324b
Luana Araújo <f.araujoluana@gmail.com>
2024-01-08 22:09:13 -0300

    Encrypt password

--- services/AuthService.py
+++ services/AuthService.py
@@ -5,3 +5,7 @@ pwd_context = CryptContext(schemes=["bcrypt"])
 
 def encrypt_password(password):
     return pwd_context.hash(password)
+
+
+def verify_password(password, encrypted_password):
+    return pwd_context.verify(password, encrypt_password)
