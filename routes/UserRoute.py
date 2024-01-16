a5bab5d0e88af04fbfc5746bc370d2f4e9f33092
Luana Araújo <f.araujoluana@gmail.com>
2024-01-16 17:43:34 -0300

    UPDATE UserRoute.py

--- routes/UserRoute.py
+++ routes/UserRoute.py
@@ -1,14 +1,16 @@
-from fastapi import APIRouter, Body, HTTPException, Depends, Header
+from fastapi import APIRouter, Body, HTTPException, Depends, Header, UploadFile
 from models.UserModel import UserCreateModel
 from services.UserService import register_user, find_current_user
 from middlewares.JWTMiddleware import verify_token
 from services.AuthService import decode_token_jwt
 
+
 router = APIRouter()
 
 @router.post("/", response_description='Route to create a new user')
-async def route_create_new_user(user: UserCreateModel = Body(...)):
+async def route_create_new_user(file: UploadFile, user: UserCreateModel = Body(...)):
     try:
+        print(file)
         result = await register_user(user)
     
         if not result['status'] == 201:
@@ -21,7 +23,7 @@ async def route_create_new_user(user: UserCreateModel = Body(...)):
         
 
 @router.get("/me", response_description='Route to search info from the current user', dependencies=[Depends(verify_token)])
-async def search_info_current_user(Authorization: str = Header(default='')):
+async def search_current_user_info(Authorization: str = Header(default='')):
     try:
         token = Authorization.split(' ')[1]
         
