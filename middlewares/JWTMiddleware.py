7b4af5c5911a15c49d5694d669e91cafa608bb7a
Luana Araújo <f.araujoluana@gmail.com>
2024-01-16 17:34:32 -0300

    CREATE middlewares

---
+++ middlewares/JWTMiddleware.py
@@ -0,0 +1,15 @@
+from fastapi import Header, HTTPException
+from services.AuthService import decode_token_jwt
+
+async def verify_token(Authorization : str = Header(default='')):
+    if not Authorization.split(' ')[0] == 'Bearer':
+        raise HTTPException(status_code=401, detail="Token required for authentication")
+    
+    token = Authorization.split(' ')[1]
+    
+    payload = decode_token_jwt(token)
+    
+    if not payload:
+        raise HTTPException(status_code=401, detail="Invalid token")
+        
+    return payload
